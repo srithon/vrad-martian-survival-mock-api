d1ef5f7dec7fa8eb7e0075463ab2ddc06aa70dc5
Sridaran Thoniyil <sri7thon@gmail.com>
2021-02-25 16:17:01 -0500

    Mirror vrad_comms API

---
+++ vrad_comms.py
@@ -0,0 +1,19 @@
+def getChargers() -> list[list[float]]:
+    """Gets list of charger coordinates
+    :returns: Returns an array of arrays (not tuples) for each discovered charger's float coordinates each with the form [x, z]
+
+    >>> vrad_comms.getChargers()
+    [[1.5, 2.0], [2.5, 4.0]]
+
+    """
+    return [[]]
+
+def getMines() -> list[list[float]]:
+    """Gets list of mine coordinates
+    :returns: Returns an array of arrays (not tuples) for each discovered mine's float coordinates each with the form [x, z]
+
+    >>> vrad_comms.getMines()
+    [[1.5, 2.0], [2.5, 4.0]]
+
+    """
+    return [[]]
