618e10a0876e59ae74384477d55f6fa9df8c67f1
Sridaran Thoniyil <sri7thon@gmail.com>
2021-03-04 13:48:02 -0500

    Add toggleTeleOps function for robot1

--- robot1.py
+++ robot1.py
@@ -1,1 +1,11 @@
 from _robot import *
+
+def toggleTeleOps(toggle: bool):
+    """Sets the robot's teleop capabilities to be on or off
+
+    :toggle: if true, enable teleop; if false, disable teleop
+    :returns: None
+
+    """
+    pass
+
